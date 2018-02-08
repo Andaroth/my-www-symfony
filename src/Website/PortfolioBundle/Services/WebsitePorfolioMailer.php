4a31ac3806948901fd61ee264356713f34dea920
Anda <ax.fiolle@gmail.com>
2018-02-08 16:20:18 +0100

    wip mailer (get post data)

--- src/Website/PortfolioBundle/Services/WebsitePorfolioMailer.php
+++ src/Website/PortfolioBundle/Services/WebsitePorfolioMailer.php
@@ -2,10 +2,12 @@
 
 namespace Website\PortfolioBundle\Services\Mailer;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class WebsitePortfolioMailer
 {
-  private $mailer, $name, $from, $msg;
-  public function __construct(\Swift_Mailer $mailer,$name,$from,$msg)
+  private $mailer;
+  public function __construct(\Swift_Mailer $mailer)
   {
     $this->mailer = $mailer;
   }
@@ -14,8 +16,33 @@ class WebsitePortfolioMailer
     ->setFrom($from)
     ->setTo('ax.fiolle@gmail.com')
     ->setBody(
-        $this->render($msg),
-        'text/plain');
+      $this->render($msg),
+      'text/plain');
     return $mailer->send($message);
   } // sendMail() end
+  public function prepareMail(Request $request)
+  {
+    $defaultData = array(
+      'name' => 'no name',
+      'email' => 'no mail',
+      'msg' => 'no msg'
+    );
+    $form = $this->createFormBuilder($defaultData)
+    ->add('name', TextType::class)
+    ->add('email', EmailType::class)
+    ->add('msg', TextareaType::class)
+    ->add('send', SubmitType::class)
+    ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $data = $form->getData();
+      $name = $data['name']; 
+      $from = $data['email'];
+      $msg = $data['msg'];
+    }
+    // next
+    $this->sendMail()
+  } // prepareMail end
 } // class end
