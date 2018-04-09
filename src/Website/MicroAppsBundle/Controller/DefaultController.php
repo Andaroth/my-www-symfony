29ed75f307e5e048f900897536c3c3a840d36de8
Andaroth <ax.fiolle@gmail.com>
2018-04-09 14:34:20 +0200

    upd

--- src/Website/MicroAppsBundle/Controller/DefaultController.php
+++ src/Website/MicroAppsBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace Website\MicroAppsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    public function indexAction()
+    {
+
+    }
+
     public function shoeAction()
     {
         return $this->render('WebsiteMicroAppsBundle:Default:shoeconceptor/index.html.php');
+    }
+
+    public function pacpouleAction()
+    {
+        return $this->render('WebsiteMicroAppsBundle:Default:pacpoule/index.html.php');
+    }
+
+    public function cookiemasterAction()
+    {
+        return $this->render('WebsiteMicroAppsBundle:Default:cookiemaster/index.html.php');
     }
 }
